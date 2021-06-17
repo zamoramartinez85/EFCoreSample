751579688343eb82cf4ff2f8ca6b0f221317f687
David Zamora <zamoramartinez85@gmail.com>
2021-06-17 13:27:21 +0200

    Primera subida del proyecto al repositorio

---
+++ SamuraiApp.UI/Program.cs
@@ -0,0 +1,41 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _samuraiContext = new SamuraiContext();
+        static void Main(string[] args)
+        {
+            _samuraiContext.Database.EnsureCreated();
+            GetSamurais("Before add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+
+            Console.WriteLine("Press any key....");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            Samurai samurai = new Samurai() { Name = "Prueba" };
+            _samuraiContext.Samurais.Add(samurai);
+            _samuraiContext.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            List<Samurai> samurais = _samuraiContext.Samurais.ToList();
+            Console.WriteLine($"{text} Samurai count in {samurais.Count}");
+
+            foreach(Samurai samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
